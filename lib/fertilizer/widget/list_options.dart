9654552d6012f9755228cbbf4528185e114911ad
emanuel mejia <emanuel.1701910366@ucaldas.edu.co>
2022-11-17 09:40:28 -0500

    changes about calculte data fertilizer

---
+++ lib/fertilizer/widget/list_options.dart
@@ -0,0 +1,26 @@
+import 'package:agri_mop/utils/card_options.dart';
+import 'package:agri_mop/utils/use_navigation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+const String description =
+    "";
+
+class ListOptions extends StatelessWidget {
+  const ListOptions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: 1,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      separatorBuilder: (_, index) => const Divider(height: 30),
+      itemBuilder: (_, index) => CardOptions(
+        icon: FontAwesome.leaf,
+        title: "Analizar Fertilizantes",
+        description: description,
+        onTap: () async => useNavigatePushName(context, "fertilizerView"),
+      ),
+    );
+  }
+}
