887f59281b9a5105911a94a00bc61c37c6c2df18
MarcoContreras2323 <108748797+MarcoContreras2323@users.noreply.github.com>
2022-11-20 13:32:49 -0500

    Documentation in the humidity model and database structure

--- lib/Models/Fertilizer.dart
+++ lib/Models/Fertilizer.dart
@@ -1,15 +1,41 @@
+const String tableDataFertilizers = 'Fertilizers';
+
+///Class containing all the attributes or fields of the categories table.
+class DataFieldsFertilizers {
+  static final List<String> values = [id, name, value];
+  static const String id = 'id';
+  static const String name = 'name';
+  static const String value = 'value';
+}
+
+///Fertilizer model with the attributes, in charge of converting and returning the information from the categories table in json format.
 class Fertilizer {
-  int id;
+  int? id;
   String name;
+  String value;
 
-  Fertilizer({required this.id, required this.name});
+  Fertilizer({required this.id, required this.name, required this.value});
 
-  Map<String, dynamic> toMap(){
-    return {'id': id, 'name': name};
-  }
+  Fertilizer copy({
+    int? id,
+    String? name,
+    String? description,
+  }) =>
+      Fertilizer(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        value: description ?? value,
+      );
 
-  @override
-  String toString() {
-    return 'Fertilizer{id: id, name: name}';
-  }
+  static Fertilizer fromJson(Map<String, Object?> json) => Fertilizer(
+        id: json["id"] as int,
+        name: json["name"] as String,
+        value: json["value"] as String,
+      );
+
+  Map<String, Object?> toJson() => {
+        DataFieldsFertilizers.id: id,
+        DataFieldsFertilizers.name: name,
+        DataFieldsFertilizers.value: value,
+      };
 }
