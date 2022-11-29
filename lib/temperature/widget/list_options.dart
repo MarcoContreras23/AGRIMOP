417f65b60e12e142e24917b1e6559fd4ea1743bc
tatianagh <carol.1701613339@gmail.com>
2022-11-29 09:00:52 -0500

    The temperature controller was created, the temperature view, the route to analyze temperature was updated

--- lib/temperature/widget/list_options.dart
+++ lib/temperature/widget/list_options.dart
@@ -8,7 +8,7 @@ import 'package:flutter_vector_icons/flutter_vector_icons.dart';
   List<ListSectionModel> listOptions=[
     const ListSectionModel(icon: FontAwesome.leaf, title: 'Analizar fertilizantes', description: '', route: 'fertilizerView'),
     const ListSectionModel(icon: FontAwesome.shopping_bag, title: 'Analizar humedad', description: '', route: 'humidityView'),
-    const ListSectionModel(icon: FontAwesome.cloud, title: 'Analizar temperatura', description: '', route: 'temperatureView'),
+    const ListSectionModel(icon: FontAwesome.shopping_bag, title: 'Analizar temperatura', description: '', route: 'temperatureView'),
   ];
 // CLASS TO SHOW OPTIONS DIALOGS ABOUT FERTILIZER
 class ListOptions extends StatelessWidget {
